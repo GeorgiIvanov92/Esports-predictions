2b5f65a32165ec5ec4657efb394b3ff23a80cc37
Georgi Ivanov <georgi.i@sbtech.com>
2023-04-01 14:44:45 +0300

    Initial web app commit

--- EsportStats/Program.cs
+++ EsportStats/Program.cs
@@ -1,18 +1,25 @@
 using EsportStats.Areas.Identity;
 using EsportStats.Data;
-using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Authorization;
-using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Identity.UI;
 using Microsoft.EntityFrameworkCore;
+using Azure.Identity;
+using Azure.Security.KeyVault.Secrets;
 
 var builder = WebApplication.CreateBuilder(args);
 
+var configuration = builder.Configuration;
+var keyVaultUrl = configuration["KeyVaultUri"];
+
+var credential = new DefaultAzureCredential();
+var client = new SecretClient(new Uri(keyVaultUrl), credential);
+
+var secret = client.GetSecret("ConnectionStrings--DefaultConnection");
+var connectionString = secret.Value;
+
 // Add services to the container.
-var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseSqlServer(connectionString));
+    options.UseSqlServer(connectionString.Value));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
     .AddEntityFrameworkStores<ApplicationDbContext>();
